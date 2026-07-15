6f55ac17fe24e2cd0133b0eb72bb7a41638be600
NoobNotFound <82730163+NoobNotFound@users.noreply.github.com>
2026-07-15 20:53:02 +0530

    Add a shared project

---
+++ samples/FluentKit.Sample.Shared/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FluentKit.Sample.Shared.MyFeature.Pages;
+
+public class Page1Model : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
